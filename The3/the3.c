fa225d29c3927bdfcd7cdc8b08f39285da1b9e52
Fatih Emre Gunes <fatih.gunes_01@metu.edu.tr>
2023-08-26 17:32:40 +0300

    Add files via upload

---
+++ The3/the3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "the3.h"
+
+/*
+    Create a cache by reading inputs from stdin
+*/
+Cache* createCache()
+{
+    return NULL;
+}
+
+/*
+    Prints cache information and its contents
+*/
+void printCache(Cache* cache)
+{
+
+}
+
+
+/*
+    Adds media to the cache
+*/
+CacheNode* addMediaRequest(Cache* cache, Media media, char* domain)
+{
+    return NULL;
+}
+
+/*
+    Finds the corresponding media's node in the cache
+    Return NULL if it is not found
+*/
+CacheNode* findMedia(Cache* cache, char* name)
+{
+    return NULL;
+}
+
+/*
+    Finds a media from its name and deletes it's node from the cache
+    Do not do anything if media is not found
+*/
+void deleteMedia(Cache* cache, char* name)
+{
+    
+}
+
